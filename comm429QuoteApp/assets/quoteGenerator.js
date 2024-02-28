7456a2e1dfa813eb6aadad8d42004f1b3b99bc5e
Matt00002 <bellmf@clarkson.edu>
2024-02-28 17:26:28 -0500

    links

--- comm429QuoteApp/assets/quoteGenerator.js
+++ comm429QuoteApp/assets/quoteGenerator.js
@@ -2,7 +2,7 @@ var quotes;
 var filter = "any";
 
 async function init() {
-	const requestURL = "assets/quotes.json";
+	const requestURL = "assets/quotes.JSON";
 	const request = new Request(requestURL);
 
 	const response = await fetch(request);
