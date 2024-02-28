989ebd4d4867e344beee5e25f299b8f7c82d3ac1
Matt00002 <bellmf@clarkson.edu>
2024-02-28 16:40:14 -0500

    add QuoteApp

---
+++ comm429QuoteApp/assets/quoteGenerator.js
@@ -0,0 +1,57 @@
+var quotes;
+var filter = "any";
+
+async function init() {
+	const requestURL = "assets/quotes.json";
+	const request = new Request(requestURL);
+
+	const response = await fetch(request);
+	quotes = await response.json();
+	
+	generateRandomQuote();
+}
+
+function generateRandomQuote(){
+	
+	setCategory();
+	
+	var filteredQuotes = quotes["quotes"];
+	
+	if(filter != "any"){
+		
+		filteredQuotes = quotes["quotes"].filter(quote => quote.category === filter);
+		
+	}
+	
+	rand = Math.floor(Math.random() * filteredQuotes.length);
+	
+	//console.log(rand);
+	//console.log(quotes["quotes"][rand])
+	//console.log(quoteFactory(quotes["quotes"][rand]))
+	
+	document.getElementById('main').insertAdjacentHTML('beforeend', quoteFactory(filteredQuotes[rand]));
+	
+}
+
+function quoteFactory(quote){
+	
+	const quoteHTML =
+	`<article class="quote">
+	<buttonbutton class="close" onclick="this.parentElement.remove();">&times;</button>
+	<blockquote>
+		<p>${quote.quote}</p>
+		<footer>-${quote.author}</footer>
+	</blockquote>
+</article>`;
+	
+	return quoteHTML;
+	
+}
+
+function setCategory(){
+	
+	var selected = document.getElementById('select').value;
+	
+	filter = selected;
+	
+}
